1480cb54aa5cdd70e80f2c7e0ff62b719acd3160
Rick van Dijk <rickavdijk@gmail.com>
2018-07-17 15:13:19 +0200

    Exercise to practice using Stack and Queue IEnumerables.

---
+++ LumberFood/LumberFood/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LumberFood
+{
+    public partial class Form1 : Form
+    {
+        private Queue<Lumberjack> breakfastLine = new Queue<Lumberjack>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addLumberjack_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(name.Text)) return;
+            breakfastLine.Enqueue(new Lumberjack(name.Text));
+            name.Text = "";
+            RedrawList();
+        }
+
+        private void addFlapjacks_Click(object sender, EventArgs e)
+        {
+            if (breakfastLine.Count == 0) return;
+            Flapjack food;
+            if (crispy.Checked == true)
+                food = Flapjack.Crispy;
+            else if (soggy.Checked == true)
+                food = Flapjack.Soggy;
+            else if (browned.Checked == true)
+                food = Flapjack.Browned;
+            else
+                food = Flapjack.Banana;
+
+            Lumberjack currentLumberjack = breakfastLine.Peek();
+            currentLumberjack.TakeFlapjacks(food,
+            (int)howMany.Value);
+
+            RedrawList();
+        }
+
+        private void RedrawList()
+        {
+            int lineOrder = 1;
+
+            line.Items.Clear();
+
+            foreach (Lumberjack lumberjack in breakfastLine)
+            {
+                line.Items.Add($"{lineOrder}. {lumberjack.Name}");
+                lineOrder++;
+            }
+
+            if (breakfastLine.Count == 0)
+            {
+                groupBox1.Enabled = false;
+                nextInLine.Text = "";
+            }
+            else
+            {
+                groupBox1.Enabled = true;
+                Lumberjack currentLumberjack = breakfastLine.Peek();
+                nextInLine.Text = $"{currentLumberjack.Name} has {currentLumberjack.FlapjackCount} flapjacks";
+            }
+
+        }
+
+        private void nextLumberjack_Click(object sender, EventArgs e)
+        {
+            if (breakfastLine.Count == 0) return;
+            Lumberjack nextLumberjack = breakfastLine.Dequeue();
+            nextLumberjack.EatFlapjacks();
+            nextInLine.Text = "";
+            RedrawList();
+        }
+    }
+}
---
+++ LumberFood/LumberFood/Lumberjack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LumberFood
+{
+    class Lumberjack
+    {
+        private string name;
+        public string Name { get { return name; } }
+        private Stack<Flapjack> meal;
+
+        public Lumberjack(string name)
+        {
+            this.name = name;
+            meal = new Stack<Flapjack>();
+        }
+
+        public int FlapjackCount { get { return meal.Count(); } }
+
+        public void TakeFlapjacks(Flapjack food, int howMany) {
+            // Add some number of flapjacks to the Meal stack
+            for (int i = 0; i < howMany; i++)
+            {
+                meal.Push(food);
+            }
+        }
+
+        public void EatFlapjacks() {
+            // Write this output to the console
+            string output = $"{Name}'s eating flapjacks{Environment.NewLine}";
+            foreach (Flapjack flapjack in meal)
+            {
+                output += $"{Name} ate a {flapjack.ToString().ToLower()} flapjack {Environment.NewLine}";
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
